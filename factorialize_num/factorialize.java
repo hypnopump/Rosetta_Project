89d56ee44c1b605cbf42698b3480c0c9044d8213
Bill Cheng <chun-chan.cheng@viasat.com>
2017-10-23 15:51:50 -0500

    added java for factorialize

---
+++ factorialize_num/factorialize.java
@@ -0,0 +1,13 @@
+public class factorialize {
+  public static void main(String... args) {
+    int res = factorializeCal(6);
+    System.out.println(res);
+  }
+
+  public static int factorializeCal(int n) {
+    if(n <= 1)
+      return n;
+    else
+      return n * factorializeCal(n-1);
+  }
+}
