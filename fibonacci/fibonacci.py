2b1c79f7ded3d71ce0f34aae988e5518a0ff97f4
Eric Alcaide <ericalcaide1@gmail.com>
2017-10-23 21:27:58 +0200

    fixing details @ fibonacci.py and readme @ dichotomic_search

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -2,7 +2,7 @@ def fibonacci():
     prev, actual = 0, 1
     for q in range(10):
         actual, prev = actual+prev, actual
-        print(aux)
+        print(actuañ)
 
 def main():
     fibonacci()
