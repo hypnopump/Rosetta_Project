1ef69ff27566cac8f5ab1371872b319326c1a701
Ashwin Patil <theashwinpatil@gmail.com>
2017-10-24 21:38:54 +0530

    Create hello_world.cpp
    Added C++ implementation.

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,13 @@
+/*
+ * Hello world implementation for Rosetta Stone in C++.
+ */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+  string hw = "Hello World";
+  cout<<"Hello World. "<<hw;
+  return 0;
+}
