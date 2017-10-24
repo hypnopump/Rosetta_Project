fd7e5984e9d262755735eeb6356498f233c0a31f
Eric Alcaide <ericalcaide1@gmail.com>
2017-10-24 22:05:41 +0200

    Update hello_world.rs

--- hello_world/hello_world.rs
+++ hello_world/hello_world.rs
@@ -1,13 +1,3 @@
-/*
- * The Problem
- * -----------
- * Print "Hello, World." on screen + declare a variable containing "Hello Wolrd" and print it on screen.
- *
- * Desired Output / Test Cases
- * ---------------------------
- * Hello, World. Hello World
- */
-
 fn main() {
     print!("Hello, World.");
     let hello_world = "Hello World";
