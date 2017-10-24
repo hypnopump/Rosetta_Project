31aac919d6d1dce0ff0b048d1239c0c853a5b467
Eric Alcaide <ericalcaide1@gmail.com>
2017-10-24 22:04:07 +0200

    Update fibonacci.rs

--- fibonacci/fibonacci.rs
+++ fibonacci/fibonacci.rs
@@ -1,12 +1,4 @@
-/*
- * The Problem
- * -----------
- * Print the 10 first numbers of the Fibonacci Series starting @ the second 1.
- *
- * Desired Output / Test Cases
- * ---------------------------
- * 1 2 3 5 8 13 21 34 55 89
- */
+/* Rust implementation. Prints 10 Fibonacci Series numbers */
 
 fn fibonacci(n: i32) -> i32 {
     if n <= 1 { return n;}
