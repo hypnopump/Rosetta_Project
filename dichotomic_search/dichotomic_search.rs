cf471888bd75bcbbcd93036c5f6a28eae94df027
Eric Alcaide <ericalcaide1@gmail.com>
2017-10-24 22:00:23 +0200

    Update dichotomic_search.rs

--- dichotomic_search/dichotomic_search.rs
+++ dichotomic_search/dichotomic_search.rs
@@ -1,16 +1,4 @@
-/*
- * The Problem
- * -----------
- * Find a value inside an ordered array.
- *
- * Desired Output / Test Cases
- * ---------------------------
- * ['a','b','d','e','g','h','j','l','m','q','x'] 'z' >> null
- * ['a','b','d','e','g','h','j','l','m','q','x'] 'e' >> 'e'
- * [1,2,3,5,6,10,15] 20 >> null
- * [1,2,3,5,6,10,15] 4 >> null
- * [1,2,3,5,6,10,15] 3 >> 3
- */
+/* Rust implementation for dichotomic search algorithm*/
  
 fn dicho_search<T: PartialOrd + Clone>(v: Vec<T>, val: T) -> Option<T> {
     let (med, _) = v.len().overflowing_div(2);
