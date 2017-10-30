cca865f20037c541de9af2860b0ae988b10d5a13
Malek Sahnoun <sahnoun.m@sfeir.com>
2017-10-31 00:28:39 +0100

    :gift: Add fibonacci recursive solution (javascript)

---
+++ 2_fibonacci/fibonacci-recursive.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+  if (n === 0) {
+    return 0;
+  }
+
+  if (n === 1) {
+    return 1;
+  }
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(fibonacci(i));
+}
