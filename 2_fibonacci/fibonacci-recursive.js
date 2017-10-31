05bd76e209bb8e6a8dad73a429167bc25b884adf
Eric Alcaide <ericalcaide1@gmail.com>
2017-10-31 08:18:02 +0100

    Update fibonacci-recursive.js

--- 2_fibonacci/fibonacci-recursive.js
+++ 2_fibonacci/fibonacci-recursive.js
@@ -1,15 +1,10 @@
 function fibonacci(n) {
-  if (n === 0) {
-    return 0;
-  }
-
-  if (n === 1) {
-    return 1;
-  }
-
+  if (n === 0) {return 0;}
+  if (n === 1) {return 1;}
+  
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 2; i < 12; i++) {
   console.log(fibonacci(i));
 }
