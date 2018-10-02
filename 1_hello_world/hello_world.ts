6e53a30bb53e60c3b46329826bf0caa22efe39b6
Poulad Ashrafpour <poulad.pld@gmail.com>
2018-10-02 00:50:35 +0000

    Add C# and TS versions of Hello World

---
+++ 1_hello_world/hello_world.ts
@@ -0,0 +1,9 @@
+function printMessage(message: string) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+
+    // Write the second line of output that prints the contents of 'message' here.
+    console.log(message);
+}
+
+printMessage("Hello World");
